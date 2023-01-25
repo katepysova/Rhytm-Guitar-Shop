10096e4cbcd6030900eba56d6adfca57f1451812
Kateryna <katepysova97@gmail.com>
2023-01-25 20:32:51 +0100

    Finished hero section

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,17 @@ const navBtn = document.querySelector(".menu-btn");
 navBtn.addEventListener("click", () => {
   header.classList.toggle("open");
 });
+
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 0 || document.body.scrollTop > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+  }
+});
