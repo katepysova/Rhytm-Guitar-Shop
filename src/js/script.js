e8e191a273b19f59ab7e25bfcbaec6ddfaf4b34c
Kateryna <katepysova97@gmail.com>
2023-01-26 22:39:58 +0100

    Started reviews slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+import Swiper, { Pagination } from "swiper";
+
 const header = document.querySelector(".header");
 const navBtn = document.querySelector(".menu-btn");
 
@@ -17,4 +19,15 @@ window.addEventListener("resize", () => {
   if (header.classList.contains("open")) {
     header.classList.remove("open");
   }
+});
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper(".swiper.reviews__swiper", {
+  modules: [Pagination],
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-my-pagination",
+    clickable: true,
+  },
 });
